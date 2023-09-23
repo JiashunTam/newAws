3dbc0f0bbe919ae8f82dfc364133403e6b260873
JiashunTam <87274146+JiashunTam@users.noreply.github.com>
2023-09-24 01:44:31 +0800

    q

--- UniversityApp.py
+++ UniversityApp.py
@@ -177,7 +177,7 @@ def signin_page():
         
     if student_id and password:
         for row in student:
-            if row[0] == student_id and row[3] == staff_log_password:
+            if row[0] == student_id and row[3] == student:
                 print ("Login successful")
                 return "correct"
             else:
